7d8ccd8e2fe61d6b87a7c12e11068cee69ac3d22
Pedro Frohmut <pedrofrohmut@gmail.com>
2019-05-09 01:37:17 -0300

    exercicio 003

---
+++ exercicio_002.py
@@ -0,0 +1,2 @@
+nome = input("Olá. Qual é o seu nome? ")
+print(f"Bem vindo {nome}, que bom te conhecer")
---
+++ exercicio_003.py
@@ -0,0 +1,4 @@
+num1 = int(input("Informe um numero: "))
+num2 = int(input("Informe outro numero: "))
+soma = num1 + num2
+print(f"A soma de {num1} e {num2} é {soma}")
